4f22f31528302f945e41ef8f3689b31147c9f321
riteshbishnoi530 <bishnoiritesh6@gmail.com>
2025-04-03 15:59:54 +0530

    add comment

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-// ===========faq
+// ===========faq=============
 const accordionItem = document.querySelectorAll('.accordionItem')
 accordionItem.forEach(item => {
     item.addEventListener('click', () => {
@@ -17,65 +17,65 @@ const swiper = new Swiper(".swiper", {
     spaceBetween: 31,
     autoplayDisableOnInteraction: true,
     autoplay: {
-      delay: 0,
+        delay: 0,
     },
     centerSlides: true,
     breakpoints: {
-      1441: {
-        slidesPerView: 3.4,
-      },
-      1025: {
-        slidesPerView: 3,
-      },
-      769: {
-        slidesPerView: 2.5,
-      },
-      600: {
-        slidesPerView: 2,
-      },
-      320: {
-        slidesPerView: 1.5,
-      },
+        1441: {
+            slidesPerView: 3.4,
+        },
+        1025: {
+            slidesPerView: 3,
+        },
+        769: {
+            slidesPerView: 2.5,
+        },
+        600: {
+            slidesPerView: 2,
+        },
+        320: {
+            slidesPerView: 1.5,
+        },
     },
-  });
+});
 
 //   ============tabs============
 const tabs = document.querySelectorAll(".tabs");
 const contents = document.querySelectorAll(".tabsInfo");
 
 tabs.forEach((tab) => {
-  tab.addEventListener("click", () => {
-    const isActiveTab = document.querySelector(".tabs.active");
-    if (isActiveTab) {
-      isActiveTab.classList.remove(
-        "active",
-        "bg-transparent",
-        "text-[#00afe1]",
-        "border-[#00afe1]"
-      );
-    }
-    contents.forEach((content) => content.classList.add("hidden"));
-    contents.forEach((content) => content.classList.remove("flex"));
-    tab.classList.add(
-      "active",
-      "bg-transparent",
-      "text-[#00afe1]",
-      "border-[#00afe1]"
-    );
+    tab.addEventListener("click", () => {
+        const isActiveTab = document.querySelector(".tabs.active");
+        if (isActiveTab) {
+            isActiveTab.classList.remove(
+                "active",
+                "bg-transparent",
+                "text-[#00afe1]",
+                "border-[#00afe1]"
+            );
+        }
+        contents.forEach((content) => content.classList.add("hidden"));
+        contents.forEach((content) => content.classList.remove("flex"));
+        tab.classList.add(
+            "active",
+            "bg-transparent",
+            "text-[#00afe1]",
+            "border-[#00afe1]"
+        );
 
-    const target = tab.getAttribute("data-target");
-    const targetContent = document.getElementById(target);
-    targetContent && targetContent.classList.remove("hidden");
-    targetContent && targetContent.classList.add("flex");
-  });
+        const target = tab.getAttribute("data-target");
+        const targetContent = document.getElementById(target);
+        targetContent && targetContent.classList.remove("hidden");
+        targetContent && targetContent.classList.add("flex");
+    });
 });
 
 // ==========memberships==========
 function moveBlueBtn(direction) {
     const blueBtn = document.getElementById("blueBtn");
     if (direction === "left") {
-      blueBtn.style.transform = "translateX(0)";
+        blueBtn.style.transform = "translateX(0)";
     } else if (direction === "right") {
-      blueBtn.style.transform = "translateX(98%)";
+        blueBtn.style.transform = "translateX(98%)";
     }
-  }
+}
